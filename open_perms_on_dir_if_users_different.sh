8fefff44f48d08fe9568818ee71da5e16ac58b8e
Roscoe A. Bartlett <rabartl@sandia.gov>
2025-09-04 07:04:03 -0600

    Add more conditions to skip
    This is needed for cases like setting up root access and allowing for
    simplifed docker file logic

--- open_perms_on_dir_if_users_different.sh
+++ open_perms_on_dir_if_users_different.sh
@@ -3,14 +3,27 @@
 # Open the same read, write, execute permissions as the owning owner for all for
 # a given directory <path_to_open_up> if <existing_user> != <new_user>
 #
-#   open_perms_on_dir_if_users_different.sh <path_to_open_up> <existing_user> <new_user>
+#   open_perms_on_dir_if_users_different.sh <path_to_open_up> <existing_user>
+#   <new_user>
+#
+# If <new_user> == root, then this is skipped as well because the root user can
+# access everything already.   Also, if either <existing_user> or <new_user> are
+# empty, this is skipped.
 #
 
 path_to_open_up=$1
 existing_user=$2
 new_user=$3
 
-if [[ ! -d ${path_to_open_up} ]] ; then
+echo "Opening permsisions for '${path_to_open_up}' with existing_user='${existing_user}', new_user='${new_user}'"
+
+if [[ "${new_user}" == "root" ]] ; then
+  echo "New user = '${new_user}', skipping!"
+elif [[ "${new_user}" == "" ]] ; then
+  echo "New user = '${new_user}', skipping!"
+elif [[ "${existing_user}" == "" ]] ; then
+  echo "Existing user = '${existing_user}', skipping!"
+elif [[ ! -d ${path_to_open_up} ]] ; then
   echo "Directory does not exist ${path_to_open_up}, skipping!"
 elif [[ "${existing_user}" == "${new_user}" ]] ; then
   echo "Existing user '${existing_user}' == New user '${new_user}', skipping!"
